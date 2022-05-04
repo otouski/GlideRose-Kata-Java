6952169fb46b619739f68898afcb23cedcf28420
Massey Sewa <otouski@outlook.com>
2022-05-04 12:05:58 +0200

    Tested class for Elixir Items

---
+++ src/test/java/com/gildedrose/ElixirTest.java
@@ -0,0 +1,35 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static com.gildedrose.Utils.checkItemEqual;
+
+public class ElixirTest {
+
+
+    @Test
+    void should_DecreaseQuality_By_One_If_SellIn_Is_In_Range(){
+
+        GildedRose gildedRose=new GildedRose(new Item[] {new Elixir(5,6)});
+        gildedRose.updateQuality();
+        checkItemEqual(new Elixir( 4, 5),gildedRose.items[0]);
+    }
+
+
+    @Test
+    void should_DecreaseQuality_By_Two_If_SellIn_Is_Less_Then_Zero(){
+
+        GildedRose gildedRose=new GildedRose(new Item[] {new Elixir(0,6)});
+        gildedRose.updateQuality();
+        checkItemEqual(new Elixir( -1, 4),gildedRose.items[0]);
+    }
+
+    @Test
+    void should_Set_Quality_To_Zero_If_Quality_Is_Less_Then_0(){
+        GildedRose gildedRose=new GildedRose(new Item[] {new Elixir(3, 0) });
+        gildedRose.updateQuality();
+        checkItemEqual(new Elixir( 2, 0),gildedRose.items[0]);
+    }
+
+
+}
